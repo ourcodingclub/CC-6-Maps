b6c38cea45c4fd74c3850ecbd253149c22a101ea
johngodlee <johngodlee@gmail.com>
2018-12-29 20:29:35 +0000

    New script

--- maps_tutorial_script.R
+++ maps_tutorial_script.R
@@ -1,36 +1,28 @@
-# Coding Club Maps Tutorial Script
-# https://ourcodingclub.github.io/2016/12/11/maps_tutorial.html
-# John Godlee
-# ourcodingclub@gmail.com
-# 2017_11_09
+# ggmap_fix_script
+rm(list = ls())
 
 # Set working directory
-setwd("~/Downloads/CC-6-Maps-master")
+setwd("~/git_proj/CC-6-Maps")
 
-# Packages ----
-library(readr)  # For reading in files
-library(dplyr)  # For formatting and cleaning data
-library(rgdal)  # For manipulating map data
-library(raster)  # For clipping shapefile polygons
-library(devtools)  # For installing packages from altenative sources, e.g. Github
-devtools::install_github("dkahle/ggmap")
-devtools::install_github("oswaldosantos/ggsn")
-library(ggmap)  # For plotting map data, downloading map tiles from online sources
-library(ggsn)  # For adding scalebars and north arrows.
+# Load packages
+library(ggplot2)  # ggplot() fortify()
+library(dplyr)  # %>% select() filter() bind_rows()
+library(rgdal)  # readOGR() spTransform()
+library(raster)  # intersect()
+library(ggsn)  # north2() scalebar()
+library(rworldmap)  # getMap()
 
-# Import bird data ----
+# Load bird data
+vulture <- read.csv("Gyps_rueppellii_GBIF.csv", sep = "\t")
+penguin <- read.csv("Spheniscus_dermersus_GBIF.csv", sep = "\t")
 
-vulture <- read.csv("Gyps_rueppellii_GBIF.csv", sep="\t")
-penguin <- read.csv("Spheniscus_dermersus_GBIF.csv", sep="\t")
-
-# Clean up bird data
 # Keep only the columns we need
 vars <- c("gbifid", "scientificname", "locality", "decimallongitude",
-					"decimallatitude", "coordinateuncertaintyinmeters")
+	"decimallatitude", "coordinateuncertaintyinmeters")
 
 vulture_trim <- vulture %>% dplyr::select(one_of(vars))
 penguin_trim <- penguin %>% dplyr::select(one_of(vars))
-# `one_of()` is part of `select()` and selects all columns specified in `vars`
+# `one_of()` is part of `dplyr` and selects all columns specified in `vars`
 
 # Combine the dataframes
 pc_trim <- bind_rows(vulture_trim, penguin_trim)
@@ -45,36 +37,39 @@ unique(pc_trim$scientificname)
 # Clean up "scientificname" to make names consistent
 pc_trim$scientificname <- pc_trim$scientificname %>%
 	recode("Gyps rueppellii (A. E. Brehm, 1852)" = "Gyps rueppellii",
-				 "Gyps rueppellii subsp. erlangeri Salvadori, 1908" = "Gyps rueppellii",
-				 "Gyps rueppelli rueppelli" = "Gyps rueppellii",
-				 "Spheniscus demersus (Linnaeus, 1758)" = "Spheniscus demersus")
+		"Gyps rueppellii subsp. erlangeri Salvadori, 1908" = "Gyps rueppellii",
+		"Gyps rueppelli rueppelli" = "Gyps rueppellii",
+		"Spheniscus demersus (Linnaeus, 1758)" = "Spheniscus demersus")
 
 # Checking names
 unique(pc_trim$scientificname)
 # Done
 
-# Preliminary plot of bird data
-ggplot(pc_trim, aes(x = decimallongitude, y = decimallatitude, colour = scientificname)) +
+# Preliminary plot
+ggplot(pc_trim, aes(x = decimallongitude, y = decimallatitude, 
+	colour = scientificname)) +
 	geom_point()
 
-# Trim data to remove USA entries
+# Remove records in Americas
 pc_trim_us <- pc_trim %>% filter(decimallongitude > -50)
 
-# Replot
-ggplot(pc_trim_us, aes(x = decimallongitude, y = decimallatitude, colour = scientificname)) +
+# Plot again
+ggplot(pc_trim_us, aes(x = decimallongitude, y = decimallatitude, 
+	colour = scientificname)) +
 	geom_point()
 
+# Get world map
+world <- getMap(resolution = "low")
 
-# Plot using data from `maps` package ---- 
-# Get map data
-map_world <- borders("world", fill = "grey90", colour = "black")
-
-# Make plot
+# Plot data with map
 ggplot() +
-	map_world +  # Add world map
+	geom_polygon(data = world, 
+		aes(x = long, y = lat, group = group),
+		fill = NA, colour = "black") + 
 	geom_point(data = pc_trim_us,  # Add and plot species data
-						 aes(x = decimallongitude, y = decimallatitude, colour = scientificname)) +
-	coord_quickmap() +  # Define aspect ratio of the map, so it doesn't get stretched when resizing
+		aes(x = decimallongitude, y = decimallatitude, 
+			colour = scientificname)) +
+	coord_quickmap() +  # Prevents stretching when resizing
 	theme_classic() +  # Remove ugly grey background
 	xlab("Longitude") +
 	ylab("Latitude") + 
@@ -84,134 +79,110 @@ ggplot() +
 saf_countries <- c("South Africa", "Namibia", "Botswana", "Zimbabwe")
 
 # Call the vector in `borders()`
-map_saf <- borders("world", regions = saf_countries, fill = "grey90", colour = "black")
+world_saf <- world[world@data$ADMIN %in% saf_countries, ]
 
 ggplot() +
-	map_saf +  # Add map
+	geom_polygon(data = world_saf, 
+		aes(x = long, y = lat, group = group),
+		fill = NA, colour = "black") + 
 	geom_point(data = pc_trim_us,  # Add and plot speices data
-						 aes(x = decimallongitude, y = decimallatitude, colour = scientificname)) +
-	coord_quickmap() +  # Define aspect ratio of the map, so it doesn't get stretched when resizing
+		aes(x = decimallongitude, y = decimallatitude, 
+			colour = scientificname)) +
+	coord_quickmap() + 
 	xlim(8, 35) +  # Set x axis limits, xlim(min, max)
 	ylim(-35, -15) +  # Set y axis limits
 	theme_classic() +  # Remove ugly grey background
-	theme(legend.position = "top") + # Position the legend at the top of the plot + 
 	xlab("Longitude") +
 	ylab("Latitude") + 
 	guides(colour=guide_legend(title="Species"))
 
-# Plotting with `ggmap`
-
-# Make bounding box
-bbox <- c(min(pc_trim_us$decimallongitude) - 2,
-					min(pc_trim_us$decimallatitude) - 2,
-					max(pc_trim_us$decimallongitude) + 2,
-					max(pc_trim_us$decimallatitude) + 2
-)
-
-# Get map data
-map_penguin <- get_map(location = bbox, source = "stamen", maptype = "toner-lite")
-
-# Preliminary plot
-ggmap(map_penguin)
-
-# Combine with point data
-ggmap(map_penguin) +
-	geom_point(data = pc_trim_us,
-						 aes(x = decimallongitude,
-						 		y = decimallatitude,
-						 		colour = scientificname),
-						 alpha = 0.6,                     # `alpha=` sets the transparency of `geom_point()`, from 0 (transparent) to 1 (opaque)
-						 size = 2) +                      # `size=` sets the diameter of `geom_point()`
-	xlab(expression("Decimal Longitude ("*degree*")")) +  # Wrapping the label in `expression()` and using *degree* lets us add a degree symbol
-	ylab(expression("Decimal Latitude ("*degree*")"))
-```
-
-# Plotting detailed zoomed data in ggmap ----
-# Read in data
-play_areas <- read.csv("play_areas.csv")
-
-# Get map
-edi_map <- get_map(location = "Edinburgh", zoom = 12, source = "google", maptype = "hybrid")
-
-# Plot with data
-ggmap(edi_map) +
-	geom_point(data = play_areas, aes(x = long, y = lat), size = 4, colour = "#06BA00")
-
-# Using shapefiles ----
-
-# Read in data
+# Read in trout data
 brown_trout <- read.csv("Brown_Trout_GBIF_clip.csv")
 
 # Preliminary plot
-ggplot(brown_trout, mapping = aes(x = decimallongitude, y = decimallatitude)) + geom_point(alpha = 0.5)
+ggplot(brown_trout, mapping = aes(x = decimallongitude, y = decimallatitude)) +
+	geom_point(alpha = 0.5)
 
-# Define bounding box
-bbox <- c(-40, 30, 40, 85)
+# Get map data
+clipper_europe <- as(extent(-10, 32, 30, 72), "SpatialPolygons")
+proj4string(clipper_europe) <- CRS(proj4string(world))
+world_clip <- raster::intersect(world, clipper_europe)
+world_clip_f <- fortify(world_clip)
 
-# Get map tiles
-Map_trout <- get_map(location = bbox, source = "google", maptype = "terrain", zoom = 3, color = "bw")
-
-# Preliminary map
-ggmap(Map_trout) +
+# Plot map
+ggplot() + 
+	geom_polygon(data = world_clip_f, 
+		aes(x = long, y = lat, group = group),
+		fill = NA, colour = "black") + 
 	geom_point(colour = "blue", alpha = 0.5,
-						 aes(x = decimallongitude, y = decimallatitude),
-						 data = brown_trout) +
+		aes(x = decimallongitude, y = decimallatitude),
+		data = brown_trout) +
 	theme_bw() +
 	xlab("Longitude") +
-	ylab("Latitude")
+	ylab("Latitude") + 
+	coord_quickmap()
 
-# Load shapefiles
-shpData_FEOW <- readOGR(dsn = "FEOW-TNC", layer = "FEOWv1_TNC")
+# Read shapefile
+shpdata_FEOW <- readOGR(dsn = "FEOW-TNC", layer = "FEOWv1_TNC")
 
 # Check CRS
-proj4string(shpData_FEOW)
+proj4string(shpdata_FEOW)
 
 # Transform CRS
-shpData_FEOW <- spTransform(shpData_FEOW, CRS("+proj=longlat +datum=WGS84"))
+shpdata_FEOW <- spTransform(shpdata_FEOW, CRS("+proj=longlat +datum=WGS84"))
 
-# Create bounding box for clipping
-clip_box <- as(extent(min(brown_trout$decimallongitude) -15,
-											max(brown_trout$decimallongitude) + 10,
-											min(brown_trout$decimallatitude),
-											max(brown_trout$decimallatitude)), "SpatialPolygons")
+# Clip spatial polygons
+shpdata_FEOW_clip <- raster::intersect(shpdata_FEOW, clipper_europe)
 
-# Clip polygons
-shpData_FEOW_clipped <- intersect(shpData_FEOW, clip_box)
+# Test plot 
+plot(shpdata_FEOW_clip)
+str(shpdata_FEOW_clip)
 
-# Plot 
-plot(shpData_FEOW_clipped)
+# Fortify for ggplot2
+shpdata_FEOW_clip_f <- fortify(shpdata_FEOW_clip, region = "ECOREGION")
 
-# Identify ID
-str(shpData_FEOW_clipped@data)
-
-# Fortify Spatialpolygons -> dataframe
-shpData_FEOW_clipped_fort <- fortify(shpData_FEOW_clipped, region = "ECOREGION")  # this could take a while
-
-# Make a plot
+# ggplot of ecoregions
 map_FEOW <- ggplot() +
-	coord_map() +
-	geom_map(data = shpData_FEOW_clipped_fort,
-					 map = shpData_FEOW_clipped_fort,
-					 aes(x = long, y = lat, map_id = id, group = group, fill = id),
-					 color = "black", size = 0.5) +
+	geom_polygon(data = shpdata_FEOW_clip_f,
+		aes(x = long, y = lat, group = group, fill = id),
+		color = "black", size = 0.5) +
 	geom_point(colour = "red", alpha = 0.5, size = 0.5,
-						 aes(x = decimallongitude, y = decimallatitude),
-						 data = brown_trout) +
+		aes(x = decimallongitude, y = decimallatitude),
+		data = brown_trout) +
 	theme_classic() +
 	theme(legend.position="bottom") +
 	theme(legend.title=element_blank()) + 
 	xlab("Longitude") +
-	ylab("Latitude")
+	ylab("Latitude") + 
+	coord_quickmap()
 
 # Add annotations
 map_FEOW_annot <- map_FEOW +
 	annotate("rect", xmin = 20 , xmax = 35, ymin = 55, ymax = 65, fill="red", alpha=0.5) +
-	annotate("text", x = 27.5, y = 61, size = 5, label = "Restock Area")
+	annotate("text", x = 27.5, y = 61, size = 10, label = "Restock Area")
 
+# Add scalebar
 map_FEOW_scale <- map_FEOW_annot +
-	scalebar(location="topleft", data = shpData_FEOW_clipped_fort,
-					 dd2km = TRUE, dist = 500, model='WGS84',
-					 height = 0.01)
+	scalebar(location="topleft", data = shpdata_FEOW_clip_f,
+		dd2km = TRUE, dist = 250, model='WGS84',
+		height = 0.01)
 
-north2(map_FEOW_scale, x = 0.3, y = 0.4, scale = 0.1, symbol = 1)
+# Add north arrow
+map_FEOW_scale 
+north2(map_FEOW_scale, x = 0.2, y = 0.2, scale = 0.1, symbol = 1)
+
+# See how many points are in which polygons
+
+point_match <- over(
+	SpatialPoints(
+		coords = data.frame(brown_trout$decimallongitude, brown_trout$decimallatitude),
+		proj4string = CRS(proj4string(shpdata_FEOW_clip))
+		), 
+	shpdata_FEOW_clip)
+
+point_match %>%
+	group_by(ECOREGION) %>%
+	tally() %>%
+	arrange(desc(n))
+
 
